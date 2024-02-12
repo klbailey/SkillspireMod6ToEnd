b490a9d495b0b8d7ebdffd7f5da76c5daa58f21e
Kathy Bailey Hines <klbailey.k@gmail.com>
2024-02-12 10:26:15 -0700

    Corrected run()

--- Mod6PythonOOP/oop4.py
+++ Mod6PythonOOP/oop4.py
@@ -42,14 +42,17 @@ class Cheetah(BigCat):
 
     # Give the Cheetah a method called run() that accepts a BigCat object.
   def run(self, anotherBigCat):
-    # If it encounter's a Leopard run the Leopard's attack method
+    # If it encounter's a Leopard run the Leopard's pounce method; leopard will pounce and decreases cheetah health by 15
     if isinstance(anotherBigCat, Leopard):
       anotherBigCat.pounce()
       # If it encounters the Lion run the Lion's king() method; anotherBigCat object performs king action which is
       # a method defined in Lion class and does things to another BigCat object like setting attributes to 
-      # specific values
-    if isinstance(anotherBigCat, Lion):
+      # specific values in this case set to 0
+    elif isinstance(anotherBigCat, Lion):
       anotherBigCat.king(anotherBigCat)
+    # If the Cheetah runs away from any of its foes then they lose 20 points in health
+    else:
+      self.health -= 20  
     
 # Create a Leopard class that inherits from the BigCat class; set the Leopard's strength intelligence and health to 30
 class Leopard(BigCat):
@@ -60,20 +63,27 @@ class Leopard(BigCat):
     self.health = 30
 
   # Give Leopard a method king() that accepts BigCat object and sets attributes to 0
-  def king(self, anotherBigCat):
-    anotherBigCat.speed = 0
-    anotherBigCat.strength = 0
-    anotherBigCat.intelligence = 0
-    anotherBigCat.health = 0
-    anotherBigCat.durability = 0
+  # def king(self, anotherBigCat):
+  #   anotherBigCat.speed = 0
+  #   anotherBigCat.strength = 0
+  #   anotherBigCat.intelligence = 0
+  #   anotherBigCat.health = 0
+  #   anotherBigCat.durability = 0
 
-    def pounce(self, anotherBigCat):
-      # If object is Lion, run Lion's king() function
-      if isintance(anotherBigCat, Lion):
-        anotherBigCat.king(anotherBigCat)
+  def pounce(self, anotherBigCat):
+    # If object is Lion, run Lion's king() function
+    if isinstance(anotherBigCat, Lion):
+      anotherBigCat.king(anotherBigCat)
+    else:
+      # If object is not Lion but a Cheetah, it should have a 60% chance of leaving unscathed.
+      if isinstance(anotherBigCat, Cheetah):
+        if random.random() <= .60:
+          print('The cheetah is unscathed.')
+        else:
+          anotherBigCat.health -= 15
       else:
-        # If object is not lion, depete health by 15 points
-        anotherBigCat.health =+ 15
+        # If object is not lion, deplete health by 15 points
+        anotherBigCat.health -= 15
     
 # Create instance / interactions for BigCat 
 # Must have instance if I want to print out the attributes of an instance of BigCat
